027302febcd4d99e0172f23e2a2c3306fbeeed92
vatanak10 <koeurnsereyvatanak@gmail.com>
2021-12-06 18:40:41 +0700

    :construction: sidebar foundation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy{
   title = 'lms-web';
+
+  mobileQuery: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  midMenu = [
+    {
+      icon: 'library_books',
+      title: 'Book Shelf',
+      link: '/books'
+    },
+    {
+      icon: 'dashboard',
+      title: 'Genres',
+      link: '.'
+    },
+    {
+      icon: 'person',
+      title: 'Authors',
+      link: '.'
+    },
+    {
+      icon: 'assignment',
+      title: 'Issued Books',
+      link: '.'
+    },
+    {
+      icon: 'favorite',
+      title: 'Wish List',
+      link: '.'
+    },
+  ];
+
+  botMenu = [
+    {
+      icon: 'settings',
+      title: 'Settings',
+    },
+    {
+      icon: 'directions_walk',
+      title: 'Log Out',
+    },
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,37 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { BookListComponent } from './book-list/book-list.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: 'books',
+    component: BookListComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatDividerModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
